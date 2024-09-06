9fe2569d101726085b6674402635e5748dcfc2a9
skyadmin <546485299@qq.com>
2024-09-06 13:58:23 +0800

    feat: update

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': process.env // 将所有环境变量挂载到 process.env
+  },
   plugins: [
     vue(),
     vueJsx(),
